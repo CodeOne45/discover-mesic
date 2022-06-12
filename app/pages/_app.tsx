4a041ff48cde5bdd8595dc7571bc015435bf88e7
Choeurtis <choeurtis.tchounga@gmail.com>
2022-06-12 12:50:09 +0200

    Edit Home page + add react switch + Dark and light mode

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,12 +1,31 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Container from "../store";
+import React, { createContext, useState } from 'react';
+import ReactSwitch from 'react-switch';
+
+
+export const ThemeContext = createContext(null);
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [theme, setTheme] = useState('dark');
+
+  const toggleTheme = () => {
+      setTheme((curr) => (curr === 'light' ? 'dark' : 'light'));
+  };
+  
   return (
-    <Container>
-      <Component {...pageProps} />
-    </Container>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      <div id={theme}>
+        <Container>
+            <div className='switch_color'>
+              <label>{theme === 'light' ? 'Light Mode' : 'Dark Mode'}</label>
+              <ReactSwitch onChange={toggleTheme} checked={theme === 'dark'}/>
+            </div>
+          <Component {...pageProps} />
+        </Container>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
