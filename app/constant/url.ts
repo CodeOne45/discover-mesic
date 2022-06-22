b1045bf1ab5ea53c324e634255832a1edf5951e0
Aman KUMAR <57890543+CodeOne45@users.noreply.github.com>
2022-06-23 00:42:25 +0200

    fix: somme changes related to deploy

--- app/constant/url.ts
+++ app/constant/url.ts
@@ -2,4 +2,5 @@ export const thumbnailLink = (id: string) =>
   `https://img.youtube.com/vi/${id}/0.jpg`;
 
 export const FRONTEND_URL = "https://discovermesic.com" || process.env.FRONTEND_URL;
-export const API_URL = process.env.NEXT_PUBLIC_API_URL + "/api";
+//export const API_URL = process.env.NEXT_PUBLIC_API_URL + "/api";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
