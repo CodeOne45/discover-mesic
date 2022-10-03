30b28c4782c5bf8572bd97f135c04454769dd7d8
Aman KUMAR <malikaman1999@gmail.com>
2022-10-03 18:34:58 +0000

    feat: Added backend to liked and added song layout

--- app/components/users/MyMusicCard.tsx
+++ app/components/users/MyMusicCard.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import CardSong from "../cardTracks/CardSong";
 import styles from "../../styles/artist.module.css";
 import {userService} from '../../services/user.service';
+import {songService} from '../../services/music.service';
 
 interface Props {
     readonly userID?: any;
@@ -10,14 +11,21 @@ interface Props {
 
 const MyMusicCard: React.FC<Props> = ({ userID }) => {
   const [mySongs, setmySongs] = useState<any>();
+  const [addedSongs, setaddedSongs] = useState<any>();
+
   const [activeLink, setActiveLink] = useState("liked");
 
 
   useEffect(() => {
-    console.log("okay");
     (async() => {
       const { data } = (await userService.get_user_liked_playlist(userID));
       if (data.length) setmySongs(data);
+
+      const addedSongs = (await songService.songsList());
+      if(userID && addedSongs.length != 0){
+        var result = addedSongs.data.filter((x)=>x.addedBy === userID);
+        setaddedSongs(result);
+      }
     })();
   }, []);
 
@@ -30,11 +38,15 @@ const MyMusicCard: React.FC<Props> = ({ userID }) => {
         </div>
 
         <section className={styles.top_songs_container}>
-        { activeLink === "liked" ? <>{mySongs? mySongs.map((track: any) => (
-            <CardSong key={track} song={track} />
-        )): <></> }</> 
-         : <></>}
-        
+        { activeLink === "liked" ?
+          <>{mySongs? mySongs.map((track: any) => (
+              <CardSong key={track} song={track} />
+          )): <> <p> No song liked yet!</p> </> }</> 
+
+         :<>{addedSongs? addedSongs.map((track: any) => (
+          <CardSong key={track} song={track} />
+          )):
+          <><p className={styles.empty_info}> No song added ! It's never late ;)</p></> }</>}
         </section>
     </React.Fragment>
   );
