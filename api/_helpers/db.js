b880f8588fb88dcef02d5f99d2a82f9b06107c1e
CodeOne45 <malikaman1999@gmail.com>
2024-08-28 23:14:33 +0200

    feat: vercel conf v3

--- api/_helpers/db.js
+++ api/_helpers/db.js
@@ -1,4 +1,4 @@
-const config = require("config.json");
+const config = require("../config.json");
 const mongoose = require("mongoose");
 require('dotenv').config()
 
--- api/_helpers/jwt.js
+++ api/_helpers/jwt.js
@@ -1,5 +1,5 @@
 const expressJwt = require("express-jwt");
-const config = require("config.json");
+const config = require("../config.json");
 const userService = require("../models/users/user.service");
 
 module.exports = jwt;
